4776147e41da33c38cdfeecabb24cd0c549940d6
emtadeveloper <47769965+emtadeveloper@users.noreply.github.com>
2023-12-14 23:00:18 +0330

    ghjghj

--- app/utils/init_redis.js
+++ app/utils/init_redis.js
@@ -3,7 +3,7 @@ const redisClient = redisDB.createClient();
 redisClient.connect();
 redisClient.on("connect", () => console.log("connected to redis"));
 redisClient.on("ready", () => console.log("connected to redis and ready to use ....."));
-redisClient.on("error", () => console.log("RedisError : ", err.message));
+redisClient.on("error", (err) => console.log("RedisError : ", err.message));
 redisClient.on("end", () => console.log("disconnected from redis ...."));
 
 module.exports = redisClient;
