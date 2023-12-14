59ff330a6c3c106bfe6036aa4d58897fdd8b9fec
emtadeveloper <47769965+emtadeveloper@users.noreply.github.com>
2023-12-15 00:54:50 +0330

    مگکگ

--- app/router/admin/blog.js
+++ app/router/admin/blog.js
@@ -4,51 +4,91 @@ const router = require("express").Router();
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     BlogCategory:
+ *       type: object
+ *       required:
+ *         - title
+ *         - text
+ *         - short_text
+ *         - tags
+ *         - category
+ *         - image
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: "The title of the blog category"
+ *         text:
+ *           type: string
+ *           description: "The detailed text of the blog category"
+ *         short_text:
+ *           type: string
+ *           description: "A short description of the blog category"
+ *         tags:
+ *           type: string
+ *           description: "Associated tags for the category, separated by #"
+ *           example: "tag1#tag2#tag3"
+ *         category:
+ *           type: string
+ *           description: "The ID of the parent category"
+ *         image:
+ *           type: string
+ *           format: binary
+ *           description: "Image file for the blog category"
+ *       example:
+ *         title: "A Guide to Modern Web Development"
+ *         text: "Detailed article content here"
+ *         short_text: "An overview of modern web development practices"
+ *         tags: "webdev#coding#javascript"
+ *         category: "5f2b39035a83a33d2f3dfcc5"
+ *         image: (binary)
+ */
+
+/**
+ * @swagger
  * /admin/blogs/add:
  *   post:
+ *     summary: Upload an image
+ *     description: Uploads details for a blog category including an image
  *     tags:
- *       - AdminPanel
- *     summary: "Create a new blog category"
+ *     - AdminPanel
  *     consumes:
- *       - multipart/form-data
- *       - application/x-www-form-data-urlencoded
- *     parameters:
- *       - in: formData
- *         name: title
- *         type: string
- *         required: true
- *         description: "The title of the blog category"
- *       - in: formData
- *         name: text
- *         type: string
- *         required: true
- *         description: "The detailed text of the blog category"
- *       - in: formData
- *         name: short_text
- *         type: string
- *         required: true
- *         description: "A short description of the blog category"
- *       - in: formData
- *         name: tags
- *         type: string
- *         required: true
- *         description: "Associated tags for the category, separated by #"
- *         example: "tag1#tag2#tag3"
- *       - in: formData
- *         name: category
- *         type: string
- *         required: true
- *         description: "The ID of the parent category"
- *       - in: formData
- *         name: image
- *         type : file
- *         required: true
- *         description: "Image file for the blog category"
+ *     - "multipart/form-data"
+ *     produces:
+ *     - "application/json"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - text
+ *               - short_text
+ *               - tags
+ *               - category
+ *               - image
+ *             properties:
+ *               title:
+ *                 type: string
+ *               text:
+ *                 type: string
+ *               short_text:
+ *                 type: string
+ *               tags:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *               image:
+ *                 type: string
+ *                 format: binary
  *     responses:
- *       201:
- *         description: "Category created successfully."
- *       400:
- *         description: "Error in request parameters/body."
+ *       200:
+ *         description: Blog category details uploaded successfully
+ *       500:
+ *         description: Internal server error
  */
 
 router.post("/add", AdminBlogController.createBlog);
